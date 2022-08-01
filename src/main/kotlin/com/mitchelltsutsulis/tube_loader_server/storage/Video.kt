ab7a108bd2f51226c348e8976628aaea3bd36e73
mitchell <mitchell444999@gmail.com>
2022-08-01 23:37:55 +1000

    Added video data entity with paging/sorting repository

---
+++ src/main/kotlin/com/mitchelltsutsulis/tube_loader_server/storage/Video.kt
@@ -0,0 +1,26 @@
+package com.mitchelltsutsulis.tube_loader_server.storage
+
+import com.fasterxml.jackson.annotation.JsonProperty
+import java.sql.Timestamp
+import javax.persistence.*
+
+@Entity
+@Table(name="video")
+open class Video(
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    open var id: Long,
+    @Column(nullable=false)
+    open var downloaded: Boolean = false,
+    @Column(nullable=false)
+    open var queued: Boolean = true,
+    @JsonProperty("video_id")
+    @Column(nullable=false, name="video_id")
+    open var videoId: String,
+    @Column(nullable=false)
+    open var title: String,
+    @Column(nullable=false)
+    open var thumbnail: String,
+    @Column(name="downloaded_at")
+    open var downloadedAt: Timestamp? = null
+)
