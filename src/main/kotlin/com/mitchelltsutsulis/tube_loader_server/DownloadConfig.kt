b8c4e9f1fd0b88db96fa7a9bf06d9f3c58e4b16c
mitchell <mitchell444999@gmail.com>
2022-09-05 19:04:31 +1000

    Added scheduled cron config

--- src/main/kotlin/com/mitchelltsutsulis/tube_loader_server/DownloadConfig.kt
+++ src/main/kotlin/com/mitchelltsutsulis/tube_loader_server/DownloadConfig.kt
@@ -8,5 +8,6 @@ import org.springframework.context.annotation.Configuration
 @ConfigurationProperties(prefix = "download")
 data class DownloadConfig(
     var videoStorage: String = "/opt/tube_loader/videos/",
+    var downloadCron: String =  "30 * * * * *",
     var downloaderConfig: Array<String> = arrayOf()
 )
