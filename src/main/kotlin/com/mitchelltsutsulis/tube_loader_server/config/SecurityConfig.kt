f24fb430a42ef8f5de23a1c00296f8d342a0b73b
Mitchell <mitchell444999@gmail.com>
2022-09-12 20:01:01 +1000

    Added passAttemptMax for brute force protection and filter chain config

--- src/main/kotlin/com/mitchelltsutsulis/tube_loader_server/config/SecurityConfig.kt
+++ src/main/kotlin/com/mitchelltsutsulis/tube_loader_server/config/SecurityConfig.kt
@@ -3,16 +3,19 @@ package com.mitchelltsutsulis.tube_loader_server.config
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.core.userdetails.User
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.provisioning.InMemoryUserDetailsManager
+import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
-@ConfigurationProperties(prefix = "account")
+@ConfigurationProperties(prefix = "security")
 class SecurityConfig(
     var username: String = "",
-    var password: String = ""
+    var password: String = "",
+    var passwordAttemptMax: Int = 10
 ) {
     @Bean
     fun userService(passwordEncoder: BCryptPasswordEncoder): UserDetailsService {
@@ -24,6 +27,15 @@ class SecurityConfig(
                 .build()
         )
         return manager
+    }
+
+    @Bean
+    fun filter(http: HttpSecurity): SecurityFilterChain {
+        http.authorizeRequests()
+            .anyRequest().authenticated()
+            .and().csrf().disable()
+            .httpBasic()
+        return http.build()
     }
 
     @Bean
