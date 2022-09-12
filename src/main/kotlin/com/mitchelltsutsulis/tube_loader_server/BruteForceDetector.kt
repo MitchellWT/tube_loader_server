1887614683db172180f639ab6a08fd94cf5eadbd
Mitchell <mitchell444999@gmail.com>
2022-09-12 20:02:34 +1000

    Fully implemented BFD with concurrent hash map

--- src/main/kotlin/com/mitchelltsutsulis/tube_loader_server/BruteForceDetector.kt
+++ src/main/kotlin/com/mitchelltsutsulis/tube_loader_server/BruteForceDetector.kt
@@ -1,7 +1,31 @@
 package com.mitchelltsutsulis.tube_loader_server
 
-import org.springframework.stereotype.Component
+import com.mitchelltsutsulis.tube_loader_server.config.SecurityConfig
+import org.springframework.stereotype.Service
+import java.util.concurrent.ConcurrentHashMap
+import javax.servlet.http.HttpServletRequest
 
-@Component
-class BruteForceDetector {
+@Service
+class BruteForceDetector(val securityConfig: SecurityConfig) {
+    val attemptCache = ConcurrentHashMap<String, Int>()
+
+    fun loginSuccess(ipAddr: String) {
+        if (blocked(ipAddr)) return
+        attemptCache.remove(ipAddr)
+    }
+
+    fun loginFail(ipAddr: String) {
+        attemptCache.putIfAbsent(ipAddr, 0)
+        attemptCache[ipAddr] = attemptCache.getValue(ipAddr) + 1
+    }
+
+    fun getIpAddr(req: HttpServletRequest): String {
+        val proxyHeader = req.getHeader("X-Forwarded-For") ?: return req.remoteAddr
+        return proxyHeader.split(",")[0]
+    }
+
+    fun blocked(ipAddr: String): Boolean {
+        val attemptCount = attemptCache[ipAddr] ?: return false
+        return attemptCount >= securityConfig.passwordAttemptMax
+    }
 }
